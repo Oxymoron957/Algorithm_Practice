58b42f4d04605ce770072097c939988a0522f1ce
KWON TAE HOON <h9579895@gmail.com>
2021-07-10 02:03:42 +0900

    Update boj3190.py

--- boj/boj3190.py
+++ boj/boj3190.py
@@ -6,6 +6,7 @@ https://www.acmicpc.net/problem/3190
 N = int(input())
 K = int(input())
 
+# Map Generation 
 map = [['0']*N for _ in range(N)]
 
 for i in range(K):
@@ -18,40 +19,57 @@ for i in range(T):
     time, direction = [x for x in input().split(' ')]
     queue.append([int(time), direction])
 
-for _ in map:
-    print(_)
-print()
+# for _ in map:
+#     print(_)
+# print()
 
 # w a s d
 cur_pos = [0,0,'d']
 time = 0
+trail = []
+trail_len = 1
 while True:
+    
     # check if I have to move
-    if queue[0][0] == time: 
-        
+    if len(queue) > 0 and queue[0][0] == time : 
         next_direction = queue.pop(0)[1]
         if next_direction == 'L':
             if cur_pos[2] == 'w':
                 cur_pos[2] = 'a'
-            if cur_pos[2] == 'a':
+            
+            elif cur_pos[2] == 'a':
                 cur_pos[2] = 's'
-            if cur_pos[2] == 's':
+
+            elif cur_pos[2] == 's':
                 cur_pos[2] = 'd'
-            if cur_pos[2] == 'd':
+
+            elif cur_pos[2] == 'd':
                 cur_pos[2] = 'w'
+        
         if next_direction == 'D':
             if cur_pos[2] == 'w':
                 cur_pos[2] = 'd'
-            if cur_pos[2] == 'a':
+
+            elif cur_pos[2] == 'a':
                 cur_pos[2] = 'w'
-            if cur_pos[2] == 's':
+
+            elif cur_pos[2] == 's':
                 cur_pos[2] = 'a'
-            if cur_pos[2] == 'd':
-                #print('check')
+
+            elif cur_pos[2] == 'd':
                 cur_pos[2] = 's'
-            
+    #print(cur_pos[1], cur_pos[0])
+    
+    if map[cur_pos[1]][cur_pos[0]] == '1':
+        map[cur_pos[1]][cur_pos[0]] = '0'
+        trail_len += 1
+    trail.insert(0,cur_pos[:-1])
+
+    #print(cur_pos, trail, trail_len)
+
     # move 
     cur_direction = cur_pos[2]
+
     if cur_direction == 'w':
         cur_pos[1] -= 1
     if cur_direction == 'a':
@@ -61,13 +79,27 @@ while True:
     if cur_direction == 'd':
         cur_pos[0] += 1
     
+    
 
     # check if it ends
-    if not (0 <= cur_pos[0] <= N and 0 <= cur_pos[1] <= N ):
-        print(cur_pos)
+    flag = False
+    for i in range(trail_len):
+        #print(cur_pos[:2], trail[i])
         
-        #print(time)
+        if cur_pos[:2] == trail[i]:
+            print(time+1)
+            flag = True
+            break
+        if flag:
+            break
+    if flag:
         break
-    #print(cur_pos)
+    if not (0 <= cur_pos[0] <= N-1 and 0 <= cur_pos[1] <= N-1 ):
+        print(time+1)
+        break
+    
+            
+    #print()
+
     time += 1
 
