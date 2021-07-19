a6369a720d66c47b1ae3f5c4de5080bd9b401313
KWON TAE HOON <h9579895@gmail.com>
2021-07-20 01:17:15 +0900

    코딩테스트 공부

--- boj/boj11066.py
+++ boj/boj11066.py
@@ -11,31 +11,56 @@ https://www.acmicpc.net/problem/11066
 
 """
 
-T = int(input())
+import sys
+input = sys.stdin.readline
+MAX = sys.maxsize
 
-for _ in range(T):
-    cost = 0
-    K = int(input())
-    num_arr = list(map(int, input().split()))
-    num_arr.sort()
-    print(num_arr)
-    index = 0
-    while True:
-        if index+2 >= len(num_arr): 
-            index = 0
-        if num_arr[index]+num_arr[index+1] > num_arr[index+2]:
-            cost += num_arr[index]+num_arr[index+1]
-            num_arr[index:index+2] = [num_arr[index]+num_arr[index+1]]
-            index += 1
-        else:
-            index = 0
+t = int(input())
+
+def solve():
+    n = int(input())
+    file = list(map(int, input().split()))
+    dp = [[0]*n for _ in range(n)]
+    sum = [0]
+    for f in file:
+        sum.append(sum[-1]+f) # index-1의 값을 더한 sum을 가지는 배열
+        
+    for d in range(1,n): # 파일의 수 만큼 
+        for i in range(n-d): # 남은 파일의 수 만큼
+            j= d+i 
+            dp[i][j] = MAX # dp[i][j] => i~j까지의 최소 합 
+            for k in range(i,j): # 중간에 최소값이 존재하는 k가 있다면 dp[i][j]에 해당 답을 추가한다. 
+                dp[i][j] = min(dp[i][j],dp[i][k]+dp[k+1][j]+sum[j+1]-sum[i]) 
+    print(dp[0][n-1])
+
+for i in range(t): 
+    solve()
+
+# T = int(input())
+
+# for _ in range(T):
+#     cost = 0
+#     K = int(input())
+#     num_arr = list(map(int, input().split()))
+#     num_arr.sort()
+#     print(num_arr)
+#     index = 0
+#     while True:
+#         if index+2 >= len(num_arr): 
+#             index = 0
+#         if num_arr[index]+num_arr[index+1] > num_arr[index+2]:
+#             cost += num_arr[index]+num_arr[index+1]
+#             num_arr[index:index+2] = [num_arr[index]+num_arr[index+1]]
+#             index += 1
+#         else:
+#             index = 0
 
 
-        if len(num_arr) <= 3:
-            cost += sum(num_arr)
-            print(cost)
+#         if len(num_arr) <= 3:
+#             cost += sum(num_arr)
+#             print(cost)
 
         
-        print(num_arr, index)
-        K-=1
+#         print(num_arr, index)
+#         K-=1
 
--- programmers/programmers77886.py
+++ programmers/programmers77886.py
@@ -1,35 +1,55 @@
 """
 110 옮기기
 https://programmers.co.kr/learn/courses/30/lessons/77886
+
+1. 문자열에서 110을 모두 제거해야한다
+2. 제거한 110을 적절한 위치에 삽입한다.
 """
 
 def solution(s):
     answer = []
-
     for inputStr in s:
-        while True:
-            print(inputStr)
-            changed = False
-            if '110' in inputStr and '111' in inputStr:
-                index110 = inputStr.rfind('110')
-                if index110-1 < len(inputStr) and inputStr[index110-1] == 1:
-                     inputStr[index110+1] = 0
-                     inputStr[index110+2] = 1
-                # index110 = inputStr.rfind('110')
-                # index111 = inputStr.index('111')
-                # if index110 > index111:
-                #     # inputStr = list(inputStr)
-                #     # inputStr[index110] = '1'
-                #     # inputStr[index111] = '0'
-                #     # inputStr = ''.join(inputStr)
-                    
-                #     changed = True
-            if not changed:
-                answer.append(inputStr)
+        # Stack을 이용해서 inputStr의 생기는 모든 110을 제거한다. 
+        count110 = 0
+        stack = []
+        for c in inputStr:
+            if c == '0' and len(stack) >= 2 and stack[-2:] == ['1','1']:
+                stack.pop()
+                stack.pop()
+                count110+=1
+            else:
+                stack.append(c)
+        # 가장 뒤에 있는 0 위치에 삽입 or 0이 없다면 1 앞에 삽입
+        # print(stack)
+        isZeroExist = False
+        for i in range(len(stack)-1, -1, -1):
+            if stack[i] == '0':
+                answer.append(''.join(stack[:i+1] + ['1','1','0']*count110 + stack[i+1:]))
+                isZeroExist = True
                 break
+        if not isZeroExist: 
+            answer.append(''.join(['1','1','0']*count110 + stack))
+        
     return answer
 
+# def solution(s):
+#     answer = []
+#     for inputStr in s:
+#         while True:
+#             isChanged = False
+#             if '1100' in inputStr:
+#                 index1100 = inputStr.index('1100')
+#                 inputStr = inputStr[:index1100] + '0110' + inputStr[index1100+4:]
+#                 isChanged = True
+#             if '1110' in inputStr:
+#                 index1110 = inputStr.index('1110')
+#                 inputStr = inputStr[:index1110] + '1101' + inputStr[index1110+4:]
+#                 isChanged = True
+#             if not isChanged:
+#                 answer.append(inputStr)
+#                 break
+#     return answer
 
 
-print(solution(["1110","100111100","0111111010"]))
+print(solution(["000", "1110","100111100","0111111010"]))
     
