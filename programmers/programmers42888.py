6112b8cb26bfb709986f988ba9897b8d687f5947
KWON TAE HOON <h9579895@gmail.com>
2021-09-07 17:01:21 +0900

    0907CondingStudy

--- programmers/programmers42888.py
+++ programmers/programmers42888.py
@@ -3,8 +3,28 @@
 https://programmers.co.kr/learn/courses/30/lessons/42888
 """
 
+from collections import deque
+
 def solution(record):
-    answer = []
-    
+    users = {}
+    dq = deque()
+
+    for r in record:
+        orders = r.split()
+        if orders[0] == "Enter":
+            dq.append("들어왔습니다. " + orders[1])
+            users[orders[1]] = orders[2]
+        elif orders[0] == "Leave":
+            dq.append("나갔습니다. " + orders[1])
+        elif orders[0] == "Change":
+            users[orders[1]] = orders[2]
+
+    answer = [] 
+    while dq:
+        user_info = dq.popleft().split()
+        answer.append(users[user_info[1]]+ "님이 " + user_info[0])
     return answer
 
+            
+
+print(solution(["Enter uid1234 Muzi", "Enter uid4567 Prodo","Leave uid1234","Enter uid1234 Prodo","Change uid4567 Ryan"]))
